966191cdf5e5ebbb54f566a0fd5ac785a10fbab5
ShikaSan <68810944+odakyu2004@users.noreply.github.com>
2022-07-25 14:45:17 +0900

    Update PlayerBot.cpp

--- sample/src/PlayerBot.cpp
+++ sample/src/PlayerBot.cpp
@@ -16,7 +16,6 @@ using namespace std;
 PlayerBot::PlayerBot(void ) {
 	this->setCurrentLocation(nullptr);
 	this->setCount(0);
-	this->setBefore(0);
 }
 
 PlayerBot::~PlayerBot() {
@@ -29,70 +28,73 @@ PlayerBot::~PlayerBot() {
  */
 bool PlayerBot::findGoal(Dungeon * dungeon)
 {
+	/* 4 方向への隣接頂点への移動を表すベクトル */
+	const int dx[5] = { 0,1,2,3,4};
+	int next=0;
 	bool rtv(false);
-	if(getBefore()!=0){
-		if(getCount()<=getBefore()){
-			findGoal2(dungeon);
-		}
-	}
+	vector<int> dist(dungeon->getSize(), -1); 
+	dist[0] = 0;
+	vector<int> prev(dungeon->getSize(), -1);
+	queue<int> que;
+	que.push(0);
 	
-	answer=anstmp;
+	while(!que.empty()){
+		int current_pos = que.front(); 
+	    int x = current_pos;
+		que.pop();
+		
+		for (int direction = 0; direction < 4; ++direction) {
+			if(dx[direction]==0 ){
+				if(dungeon->getTile(x)->getNorth()!= nullptr ){
+					next=x-dungeon->length;
+					cout << "N" << endl;
+				}
+			}
+			if(dx[direction]==1 ){
+				if(dungeon->getTile(x)->getEast()!= nullptr ){
+					next=x+1;
+					cout << "E" << endl;
+				}
+			}
+			if(dx[direction]==2 ){
+				if(dungeon->getTile(x)->getSouth()!= nullptr ){
+					next=x+dungeon->length;
+					cout << "S" << endl;
+				}
+			}
+			if(dx[direction]==3 ){
+				if(dungeon->getTile(x)->getWest()!= nullptr ){
+					next=x-1;
+					cout << "W" << endl;
+				}
+			}
+			if(dx[direction]==4 ){
+				if(dungeon->getTile(x)->getWarp()== nullptr ){
+					//none
+				}
+			}
+			
+			if(dist[next]==-1){
+				que.push(next);
+				dungeon->getTile(next)->setVisit(dungeon->getTile(x));
+				dist[next] = dist[x] + 1;
+				prev[next] = x;
+			}
+		}
+		
+		if(x==dungeon->getSize()-1){
+			break;
+		}
+		
+	}
+	/*for(unsigned int i=0;i<dungeon->getSize();i++){
+		cout << prev[i] << endl;
+	}*/
 	rtv=true;
 	
 	return rtv;
 }
 
-void PlayerBot::findGoal2(Dungeon * dungeon)
-{
-	vector<int> tmpanswer;
-
-	do{
-			int current = dungeon->que.front();
-			if(dungeon->getTile(current)->getNorth()!= nullptr ){
-				if(dungeon->getTile(current)->getVisit()==nullptr){
-					dungeon->getTile(current)->setVisit(this->getCurrentLocation());
-					cout << "N" << endl;
-					anstmp+="N";
-					tmpanswer.push_back(current);
-					current-=dungeon->width;
-				}
-			} else if(dungeon->getTile(current)->getEast()!= nullptr ){
-				if(dungeon->getTile(current)->getVisit()==nullptr){
-					dungeon->getTile(current)->setVisit(this->getCurrentLocation());
-					cout << "E" << endl;
-					anstmp+="E";
-					tmpanswer.push_back(current);
-					current +=1;
-				}
-			}else if(dungeon->getTile(current)->getSouth()!= nullptr ){
-				if(dungeon->getTile(current)->getVisit()==nullptr){
-					dungeon->getTile(current)->setVisit(this->getCurrentLocation());
-					cout << "S" << endl;
-					anstmp+="S";
-					tmpanswer.push_back(current);
-					current+=dungeon->width;
-				}
-			}else if(dungeon->getTile(current)->getWest()!= nullptr ){
-				if(dungeon->getTile(current)->getVisit()==nullptr){
-					dungeon->getTile(current)->setVisit(this->getCurrentLocation());
-					cout << "W" << endl;
-					anstmp+="W";
-					tmpanswer.push_back(current);
-					current -=1;
-				}
-			}else if(dungeon->getTile(current)->getWarp()!= nullptr ){
-				if(dungeon->getTile(current)->getVisit()==nullptr){
-					dungeon->getTile(current)->setVisit(this->getCurrentLocation());
-					cout << "Warp" << endl;
-					tmpanswer.push_back(current);
-				}
-			}
-			count++;
-			cout << dungeon->que.front() << endl;
-			dungeon->que.pop();
-	}while(dungeon->que.empty() ==false);
-	anstmp+="\n";
-}
 
 void PlayerBot::saveAnswer(void )
 {
@@ -102,3 +104,4 @@ void PlayerBot::saveAnswer(void )
 
 	return;
 }
+
